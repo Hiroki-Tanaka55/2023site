8246915d6cfda8761251d5c2554d3eab8d4af800
Hiroki Tanaka <info@tanaka-hiroki.com>
2023-12-02 11:44:59 +0900

    20231202

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,11 @@
 import { defineConfig } from 'astro/config';
+const subfolder = '/test/test/123/'
 
 // https://astro.build/config
 export default defineConfig({
+  compressHTML: false,
+  //base: subfolder,
+  //outDir: `./dist${subfolder}`,
     vite: {
         css: {
           preprocessorOptions: {
